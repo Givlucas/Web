a57509c0e008d6a3d127ac39fce9c78256f21183
lucas <givlucas@gmail.com>
2022-01-13 08:41:46 -0500

    Added showroom

--- fapp.py
+++ fapp.py
@@ -12,5 +12,9 @@ def index():
 def snapCode():
     return render_template('snapCode.html')
 
+@app.route("/showroom")
+def showroom():
+    return render_template('showroom/showroom.html')
+
 if __name__ == "__main__":
     app.run(debug=False)
