5d7805c98bc601495254544bb5b5e7f678558440
lucas <givlucas@gmail.com>
2022-02-28 16:15:06 -0500

    fixed

---
+++ auth.py
@@ -0,0 +1,17 @@
+from flask import Blueprint
+
+from flask import render_template
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return 'Login'
+
+@auth.route('/signup')
+def signup():
+    return 'Signup'
+
+@auth.route('/logout')
+def logout():
+    return 'Logout'
---
+++ routes.py
@@ -0,0 +1,17 @@
+from flask import Blueprint
+
+from flask import render_template
+
+routes = Blueprint('routes', __name__)
+
+@routes.route("/")
+def index():
+    return render_template('index.html')
+
+@routes.route("/snapCode")
+def snapCode():
+    return render_template('snapCode.html')
+
+@routes.route("/showroom")
+def showroom():
+    return render_template('general.html')
